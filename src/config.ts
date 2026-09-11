3707930f59d2da8ff84d6c331b0ba587a6425f46
TY_Ling <Yazisama123@gmail.com>
2026-09-11 01:08:34 +0000

    chore: 站名改为天音铃 / TenonSuzu

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "奏的博客",
-	subtitle: "Kanade's Notes",
+	title: "天音铃",
+	subtitle: "TenonSuzu",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
